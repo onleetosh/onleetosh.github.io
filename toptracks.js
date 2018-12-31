766366aa8dec4f24ea4f7fbd80598ca34613a593
Ross Dakin <rossdakin@users.noreply.github.com>
2018-12-31 03:57:16 -0500

    Update toptracks.js

--- toptracks.js
+++ toptracks.js
@@ -1,6 +1,6 @@
 //get top tracks api request
 $(document).ready(function() {
-    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=e368bc90b36bc0339e0bf05aad7fd4f8&format=json", function(result) {
+    $.getJSON("//ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=e368bc90b36bc0339e0bf05aad7fd4f8&format=json", function(result) {
         var topSongs = '';
         $.each(result.tracks.track, function(i, item) {
             topSongs += "<br><p>" + item.artist.name + "<a href=" + item.url + " target='_blank'><br> " + item.name + " </a><br> " + "Number of streams : " +item.playcount + "</p>";                     });
